4b7cdce1cb269c4c9b5ae6e2cf43d4e5e676a64b
NiktabE <maniktab@gmail.com>
2021-02-08 21:47:21 +1300

    python wraper with profiler

--- BazelTestProject/main/LsfJobPython.py
+++ BazelTestProject/main/LsfJobPython.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import os
+import time
+import cProfile
 
 
 def prepare_io(list_of_files, exe_file, input_path, output_path, job_number):
@@ -15,32 +17,40 @@ def prepare_io(list_of_files, exe_file, input_path, output_path, job_number):
 
     if not os.path.isdir(output_dir):
         subprocess.run("mkdir {}".format(output_dir), shell=True, stdout=subprocess.PIPE)
-    print(">>>>>>>>>>>>>>")
-    print(output_dir)
-    # run vcf to tensor -- c++ code
-    exe = "{} {} {}".format(exe_file, to_read, output_dir)
-    subprocess.run(exe, shell=True, stdout=subprocess.PIPE)
-    print("{} is done \n".format(exe))
-    # zip output files
-    exe_2 = "tar -cjf {} {}".format(zip_output_path, output_dir)
-    subprocess.run(exe_2, shell=True, stdout=subprocess.PIPE)
-    print("{} is done \n".format(exe_2))
-    # remove residual files
-    exe_3 = "rm -r {}".format(output_dir)
-    subprocess.run(exe_3, shell=True, stdout=subprocess.PIPE)
-    print("{} is done \n".format(exe_3))
+    with open(os.path.join(output_path, input_file).replace(".vcf.gz", "_logs.txt"), "a") as log_file:
+        log_file.write("{} \n".format(input_file))
+        # run vcf to tensor -- c++ code
+        exe = "{} {} {}".format(exe_file, to_read, output_dir)
+        start = time.time()
+        subprocess.run(exe, shell=True, stdout=subprocess.PIPE)
+        end = time.time()
+        elapsed = (end - start) / 360
+        log_file.write("{} was done in {} hours \n".format(exe, elapsed))
+
+        # zip output files
+        exe_2 = "tar -cjf {} {}".format(zip_output_path, output_dir)
+        start = time.time()
+        subprocess.run(exe_2, shell=True, stdout=subprocess.PIPE)
+        end = time.time()
+        elapsed = (end - start) / 360
+        log_file.write("{} was done in {} hours \n".format(exe_2, elapsed))
+
+        # remove residual files
+        exe_3 = "rm -r {}".format(output_dir)
+        subprocess.run(exe_3, shell=True, stdout=subprocess.PIPE)
+        log_file.write("{} is done \n".format(exe_3))
 
 
 def main(argv):
-    prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2],
-               output_path=argv[3], job_number=argv[4])
-
-
-
+    if int(argv[4]) == 1:
+        prof = cProfile.Profile()
+        prof.enable()
+        prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2], output_path=argv[3], job_number=argv[4])
+        prof.disable()
+        prof.dump_stats(os.path.join(argv[2], "sample.prof"))
+    else:
+        prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2], output_path=argv[3], job_number=argv[4])
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-#python3 LsfJobPython.py .../listoffiles.vcf .../VcfImage input/TestData output/TestData/ 1
