3f4f01cc26d1ba773442c0db9f0a58e19afd8380
NiktabE <maniktab@gmail.com>
2021-02-09 22:54:40 +1300

    python wraper with profiler

--- BazelTestProject/main/LsfJobPython.py
+++ BazelTestProject/main/LsfJobPython.py
@@ -17,41 +17,47 @@ def prepare_io(list_of_files, exe_file, input_path, output_path, job_number):
 
     if not os.path.isdir(output_dir):
         subprocess.run("mkdir {}".format(output_dir), shell=True, stdout=subprocess.PIPE)
-    with open(os.path.join(output_path, input_file).replace(".vcf.gz", "_logs.txt"), "a") as log_file:
-        log_file.write("{} \n".format(input_file))
+    log_file = open(os.path.join(output_path, input_file).replace(".vcf.gz", "_logs.txt"), "a")
+    log_file.write("{} \n".format(input_file))
+
+    exe = "{} {} {}".format(exe_file, to_read, output_dir)
+    start = time.time()
+    if job_number == 0:
         # run vcf to tensor -- c++ code
-        exe = "{} {} {}".format(exe_file, to_read, output_dir)
-        start = time.time()
+        prof = cProfile.Profile()
+        prof.enable()
+        subprocess.run(exe, shell=True, stdout=subprocess.PIPE)
+        end = time.time()
+        prof.disable()
+        prof_path = os.path.join(output_path, input_file).replace(".vcf.gz", "sample.prof")
+        prof.dump_stats(prof_path)
+        elapsed = (end - start) / 360
+        log_file.write("{} was done in {} hours \n".format(exe, elapsed))
+    else:
         subprocess.run(exe, shell=True, stdout=subprocess.PIPE)
         end = time.time()
         elapsed = (end - start) / 360
         log_file.write("{} was done in {} hours \n".format(exe, elapsed))
 
-        # zip output files
-        exe_2 = "tar -cjf {} {}".format(zip_output_path, output_dir)
-        start = time.time()
-        subprocess.run(exe_2, shell=True, stdout=subprocess.PIPE)
-        end = time.time()
-        elapsed = (end - start) / 360
-        log_file.write("{} was done in {} hours \n".format(exe_2, elapsed))
+    # zip output files
+    exe_2 = "tar -cjf {} {}".format(zip_output_path, output_dir)
+    start = time.time()
+    subprocess.run(exe_2, shell=True, stdout=subprocess.PIPE)
+    end = time.time()
+    elapsed = (end - start) / 360
+    log_file.write("{} was done in {} hours \n".format(exe_2, elapsed))
 
-        # remove residual files
+    # remove residual files
 
-        exe_3 = "rm -r {}".format(output_dir)
-        log_file.write("{} started \n".format(exe_3))
-        subprocess.run(exe_3, shell=True, stdout=subprocess.PIPE)
-        log_file.write("{} was done \n".format(exe_3))
+    exe_3 = "rm -r {}".format(output_dir)
+    log_file.write("{} started \n".format(exe_3))
+    subprocess.run(exe_3, shell=True, stdout=subprocess.PIPE)
+    log_file.write("{} was done \n".format(exe_3))
+    log_file.close()
 
 
 def main(argv):
-    if int(argv[4]) == 1:
-        prof = cProfile.Profile()
-        prof.enable()
-        prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2], output_path=argv[3], job_number=argv[4])
-        prof.disable()
-        prof.dump_stats(os.path.join(argv[2], "sample.prof"))
-    else:
-        prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2], output_path=argv[3], job_number=argv[4])
+    prepare_io(list_of_files=argv[0], exe_file=argv[1], input_path=argv[2], output_path=argv[3], job_number=argv[4])
 
 
 if __name__ == "__main__":
